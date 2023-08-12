1e3963808840f2e3cebb7553da18c948cf30fb46
siemah <hmi.dayenio@gmail.com>
2023-08-12 08:16:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Push Notification Expo
+
+This Expo Plugin for WordPress is designed to simplify the integration of push notifications to all Android and iOS devices. With ease of use in mind.
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Push Notification Expo
+ * Plugin URI: https://github.com/siemah/zz-notification
+ * Description: This Expo Plugin for WordPress is designed to simplify the integration of push notifications to all Android and iOS devices. With ease of use in mind
+ * Version: 1.0.0
+ * Author: Siemah<hmi.dayenio@gmail.com>
+ * Author URI: https://github.com/siemah/
+ * 
+ */

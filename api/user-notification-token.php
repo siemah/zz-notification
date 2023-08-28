8788c08644d54760dcecfa20fa94aea1252cfeef
siemah <hmi.dayenio@gmail.com>
2023-08-28 09:09:00 +0100

    feat:get user loggedin using a loging cookie

--- api/user-notification-token.php
+++ api/user-notification-token.php
@@ -19,14 +19,35 @@ function zz_expo_notification_setup_rest_routes()
   );
 }
 
+
+/**
+ * Get loggedin customer
+ */
+function get_logedin_customer($cookie)
+{
+  $user = wp_parse_auth_cookie($cookie, "logged_in");
+
+  if ($user !== FALSE) {
+    $user        = get_user_by("login", $user["username"]);
+    $customer_id = $user->get("id");
+
+    return [
+      "id" => $customer_id
+    ];
+  }
+
+  return NULL;
+}
+
+
 /**
  * Save push notification token
  */
 function zz_save_vendor_push_notification_token($request)
 {
-  $user_id  = get_current_user_id();
+  $user     = get_logedin_customer($request->get_header("set-cookie"));
   $token    = $request->get_param('token');
-  $response = $response = new WP_REST_Response(
+  $response = new WP_REST_Response(
     [
       "code"   => "success",
       "status" => 200,
@@ -34,13 +55,14 @@ function zz_save_vendor_push_notification_token($request)
     200
   );
 
-  if ((new Expo)->isExpoPushToken($token) === FALSE || !!$user_id === FALSE) {
+  if ((new Expo)->isExpoPushToken($token) === FALSE || $user === NULL) {
     $response->set_status(403);
     $response->set_data([
       "status" => 403,
       "code"   => "unauthorized",
     ]);
   } else {
+    $user_id  = $user["id"];
     $is_saved = update_user_meta(
       $user_id,
       USER_PUSH_NOTIFICATION_NAME,
